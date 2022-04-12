8eadf9aa6bf4f5b59c764633e5dbc01fbb21f7ca
popai <yoshitaka.shu@gmail.com>
2022-04-12 22:44:34 +0900

    commit

--- SnakeGame/Build/SnakeGame.framework.js
+++ SnakeGame/Build/SnakeGame.framework.js
@@ -1292,7 +1292,7 @@ function _emscripten_asm_const_id(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 2307616;
+STATICTOP = STATIC_BASE + 2307680;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AIScriptingClasses_cpp();
@@ -3366,7 +3366,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 2307616;
+var STATIC_BUMP = 2307680;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
